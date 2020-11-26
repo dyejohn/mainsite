e39aa2de13f5613203f59e9cec64ba39324c6be2
John Dye <geekydevguy@gmail.com>
2020-11-26 14:47:38 -0500

    Initial Setup, menu added

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Menu/>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Menu from "@/components/Menu";
+
+export default {
+  name: 'App',
+  components: {
+    Menu,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="menu-bar"><div class="menu-item" >Home</div><div class="menu-item">Contact</div><div class="menu-item">Projects</div> </div>
+</template>
+
+<script>
+export default {
+name: "Menu"
+}
+</script>
+
+<style scoped>
+.menu-bar {
+  float: right;
+}
+
+.menu-item {
+  margin-left: 3px;
+  margin-right: 3px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  float: left;
+}
+</style>
